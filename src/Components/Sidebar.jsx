2609604864394b6bcc45e79b6f8eae808f79d1bf
marzonha <marzonhapacheco@gmail>
2024-06-21 15:01:54 -0300

    Ajustes

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,16 +1,16 @@
-import styles from './sidebar.module.css';
+import style from './sidebar.module.css';
 
 export default function Sidebar() {
   return (
-    <aside className={styles.sidebar}>
+    <aside className={style.sidebar}>
 
-     <img src="https://idocode.com.br/wp-content/uploads/2020/05/0-destaque-linguagens-de-programacao-2019.jpg" alt="" />
+     <img className={style.cover} src="https://idocode.com.br/wp-content/uploads/2020/05/0-destaque-linguagens-de-programacao-2019.jpg" alt="" />
 
-      <div>
+      <div className={style.profile}>
 
-        <img src="https://cdn.pixabay.com/photo/2023/09/26/16/43/programmer-8277836_1280.jpg" alt="" />
-        <strong></strong>
-        <span></span>
+        <img className={style.avatar} src="https://cdn.pixabay.com/photo/2023/09/26/16/43/programmer-8277836_1280.jpg" alt="" />
+        <strong>Seu nome</strong>
+        <span>Cargo Pretendido</span>
 
       </div>
         
