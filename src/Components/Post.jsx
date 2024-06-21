670906497ef3690238d885300895822feefc383a
marzonha <marzonhapacheco@gmail>
2024-06-21 16:34:02 -0300

    Criado o module do post

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,8 +1,40 @@
 
 export default function Post() {
   return (
-    <div>
+    <article>
+
+      <header>
+
+        <div>
+
+           <img src="https://cdn.pixabay.com/photo/2023/09/26/16/43/programmer-8277836_1280.jpg" alt="" />
+           <div>
+
+             <strong>Nome do Profissional</strong>
+             <span>Cargo / Função</span>
+
+           </div>
+
+        </div>
+
+      </header>
+
+      <div>
+        
+        <p>Sobre Você</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae harum exercitationem odit eaque veritatis, suscipit fugiat velit eum consequatur, aliquid tenetur consectetur quibusdam voluptas dolorum eos. Harum deleniti mollitia non?</p>
+
+        <p>Link Repositórios</p>
+        <p>Ultimo Projeto</p>
+      </div>
+
+      <form action="">
+
+       <strong>Experiencia Profissional</strong>
+
+      </form>
+
+    </article>
       
-    </div>
   )
 }
