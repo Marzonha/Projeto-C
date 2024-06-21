6716e88560d797f56281e9aaf4031449dd009abc
marzonha <marzonhapacheco@gmail>
2024-06-21 14:29:50 -0300

    teste

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,8 +1,19 @@
+import styles from './sidebar.module.css';
 
 export default function Sidebar() {
   return (
-    <div>
-      
-    </div>
+    <aside className={styles.sidebar}>
+
+     <img src="https://idocode.com.br/wp-content/uploads/2020/05/0-destaque-linguagens-de-programacao-2019.jpg" alt="" />
+
+      <div>
+
+        <img src="https://cdn.pixabay.com/photo/2023/09/26/16/43/programmer-8277836_1280.jpg" alt="" />
+        <strong></strong>
+        <span></span>
+
+      </div>
+        
+      </aside>
   )
 }
