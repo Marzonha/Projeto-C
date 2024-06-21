2c59f87c0cd618cbc97b161e33c2110e67fe361e
marzonha <marzonhapacheco@gmail>
2024-06-21 16:34:12 -0300

    estilização

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -9,13 +9,16 @@ export default function Sidebar() {
       <div className={style.profile}>
 
         <img className={style.avatar} src="https://cdn.pixabay.com/photo/2023/09/26/16/43/programmer-8277836_1280.jpg" alt="" />
+        
         <strong>Seu nome</strong>
         <span>Cargo Pretendido</span>
 
       </div>
 
         <footer>
+
           <a href="">Seu GitHub</a>
+
         </footer> 
 
       </aside>
