860f83445210c82500dfe243b0687157045844b0
Marlon <marzonhapacheco@gmail.com>
2024-06-23 17:54:49 -0300

    Estilização e Variaveis

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,25 @@ import style from './footer.module.css';
 export default function Footer() {
   return (
     <footer className={style.footer}>
-        <h1>footer</h1>
+
+        <div className={style.slider}>
+
+           <div className={style.list}>
+
+            <div className={style.item}><img src="img/slider1_1.png" alt="" /></div>
+            <div className={style.item}><img src="img/slider1_2.png" alt="" /></div>
+            <div className={style.item}><img src="img/slider1_3.png" alt="" /></div>
+            <div className={style.item}><img src="img/slider1_4.png" alt="" /></div>
+            <div className={style.item}><img src="img/slider1_5.png" alt="" /></div>
+            <div className={style.item}><img src="img/slider1_6.png" alt="" /></div>
+            <div className={style.item}><img src="img/slider1_7.png" alt="" /></div>
+            <div className={style.item}><img src="img/slider1_8.png" alt="" /></div>
+            <div className={style.item}><img src="img/slider1_9.png" alt="" /></div>
+            <div className={style.item}><img src="img/slider1_10.png" alt="" /></div>
+
+           </div>
+
+        </div>
     </footer>
   )
 }
