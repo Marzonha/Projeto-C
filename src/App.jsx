188f1f3836f224f7a6897e560b896f1f66405a48
marzonha <marzonhapacheco@gmail>
2024-06-26 16:59:51 -0300

    Add Footer e o App dnv

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import styles from './app.module.css';
+import'./styles.global.css'
+import'./app.module.css'  
+
+import Header from './Components/Header'
+import Post from './Components/Post'
+import Sidebar from './Components/Sidebar'
+import Footer from './Components/Footer';
+
+
+function App() {
+  return (
+    <>
+     <div>
+       <Header/>
+      
+       <div className={styles.wrapper}>
+         <Sidebar/>
+         <main>
+          <Post/>
+         </main>
+
+        <Footer/>
+
+       </div>
+
+     </div>
+
+    </>
+  
+  )
+
+}    
