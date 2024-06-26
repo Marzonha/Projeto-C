9f779f2215220c125a00b7af8e7bc45a95e44f2b
marzonha <marzonhapacheco@gmail>
2024-06-26 16:46:47 -0300

    Ajustes

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -12,8 +12,8 @@ export default function Post() {
            <img src="https://cdn.pixabay.com/photo/2023/09/26/16/43/programmer-8277836_1280.jpg" alt="" />
            <div className={style.authorInfo}>
 
-             <strong>Nome do Profissional</strong>
-             <span>Cargo / Função</span>
+             <strong>Marlon Pacheco</strong>
+             <span>Programador junior / Front-end  </span>
 
            </div>
 
@@ -26,8 +26,8 @@ export default function Post() {
         <p>Sobre Você</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae harum exercitationem odit eaque veritatis, suscipit fugiat velit eum consequatur, aliquid tenetur consectetur quibusdam voluptas dolorum eos. Harum deleniti mollitia non?</p>
 
-        <a>Link Repositórios</a>
-        <a>Ultimo Projeto</a>
+        <a href="https://github.com/Marzonha?tab=repositories">Link Repositórios</a>
+        <a href="https://github.com/Marzonha/Projeto-react-01.git">Ultimo Projeto</a>
         
       </div>
 
